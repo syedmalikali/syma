e1cc5000d0e52aa80d0380f680c140b63d4a48c5
Syed Malik Ali <syedmalikali@gmail.com>
2022-04-25 10:56:26 +0300

    custom script added

--- syma/inventory_management_system/doctype/quotation/quotation.js
+++ syma/inventory_management_system/doctype/quotation/quotation.js
@@ -2,7 +2,41 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Quotation', {
-	// refresh: function(frm) {
 
-	// }
 });
+
+
+var calculate_linetotal = function(frm, cdt, cdn) {
+	var child = locals[cdt][cdn];
+	frappe.model.set_value(cdt, cdn, "co4totpr", child.co4qty * child.co4unitpr);
+
+};
+var calculate_totvalue = function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	var total = 0;
+    frm.doc.item.forEach(function(d) { total += d.co4totpr; });
+    frm.set_value("co3totval", total);
+    refresh_field("co3totval");
+    var net_total=0;
+    net_total=total-50
+    frm.set_value("co3netval", net_total);
+    refresh_field("co3netval");
+
+};
+frappe.ui.form.on('Quotation Item', {
+		co4qty: function(frm,cdt, cdn){
+		calculate_linetotal(frm, cdt, cdn);
+    calculate_totvalue(frm, cdt,cdn);
+	},
+  item_remove:function(frm, cdt, cdn){
+    calculate_totvalue(frm, cdt,cdn);
+
+ },
+	co4unitpr: function(frm, cdt, cdn){
+		calculate_linetotal(frm, cdt, cdn);
+      calculate_totvalue(frm, cdt,cdn);
+	},
+  co3totval: function(frm,cdt,cdn){
+    calculate_totvalue(frm, cdt,cdn);
+  }
+})
