7a08b337d7e57ae857da42e7188616f7bee3cf72
Syed Malik Ali <syedmalikali@gmail.com>
2022-05-22 15:05:08 +0300

    22-05-2022

--- syma/inventory_management_system/doctype/in8000/in8000.js
+++ syma/inventory_management_system/doctype/in8000/in8000.js
@@ -2,7 +2,126 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IN8000', {
-	// refresh: function(frm) {
+  refresh: function(frm,cdt,cdn){
+    calculate_totvalue(frm, cdt,cdn);
+  },
+  co3disamt: function(frm,cdt,cdn){
+    calculate_totvalue(frm, cdt,cdn);
+  },
 
-	// }
 });
+
+
+var calculate_linetotal = function(frm, cdt, cdn) {
+	var child = locals[cdt][cdn];
+	frappe.model.set_value(cdt, cdn, "in9totcost", flt(child.in9qty * child.in9unitcos));
+
+};
+var calculate_totvalue = function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	var total = 0;
+    frm.doc.item.forEach(function(d) { total += d.in9totcost; });
+    frm.set_value("in8totval", total);
+    refresh_field("in8totval");
+    var net_total=0;
+    var vat =0
+    var net_total_with_vat=0
+    // net_total=total-frm.doc.co3disamt
+    vat=net_total*0.15
+    net_total_with_vat= (net_total)+(vat)
+    var total_qty = 0
+    frm.doc.item.forEach(function(d) { total_qty += d.in9qty; });
+    frm.set_value("in8totqty", total_qty);
+    refresh_field("in8totqty");
+
+    frm.set_value("in8totvalvat", net_total);
+    refresh_field("in8totvalvat");
+    frm.set_value("in8vatamount", vat);
+    refresh_field("in8vatamount");
+    frm.set_value("in8totvalvat", net_total_with_vat);
+    refresh_field("in8totvalvat");
+
+};
+frappe.ui.form.on('IN9000', {
+		in9qty: function(frm,cdt, cdn){
+		calculate_linetotal(frm, cdt, cdn);
+    calculate_totvalue(frm, cdt,cdn);
+	},
+  item_remove:function(frm, cdt, cdn){
+    calculate_totvalue(frm, cdt,cdn);
+
+ },
+	in9unitcos: function(frm, cdt, cdn){
+		calculate_linetotal(frm, cdt, cdn);
+      calculate_totvalue(frm, cdt,cdn);
+	},
+
+  in9totcost: function(frm,cdt,cdn){
+    calculate_totvalue(frm, cdt,cdn);
+  },
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// // Copyright (c) 2022, Syed Malik and contributors
+// // For license information, please see license.txt
+//
+// frappe.ui.form.on('IN8000', {
+//   refresh: function(frm,cdt,cdn){
+//     // calculate_linetotal(frm, cdt,cdn);
+//   },
+//
+//
+// });
+// var calculate_linetotal = function(frm, cdt, cdn) {
+//   	var child = locals[cdt][cdn];
+//   	frappe.model.set_value(cdt, cdn, "in9totcost", child.in9qty * child.in9unitcos);
+//     var total = 0;
+//     // frm.doc.items.forEach(function(d) { total += d.in9totcost; });
+//     frm.doc.item.forEach(function(d) { total += d.in9totcost; });
+//     // frappe.msgprint("malik");
+//     // console.log(total)
+//     frm.set_value("in8totval", total);
+//     refresh_field("in8totval");
+//   };
+// 	// refresh: function(frm) {
+//
+// 	// }
+//
+// frappe.ui.form.on('IN9000', {
+// 		in9qty: function(frm,cdt, cdn){
+// 		calculate_linetotal(frm, cdt, cdn);
+//   },
+//   in9unitcos: function(frm,cdt, cdn){
+//   calculate_linetotal(frm, cdt, cdn);
+// },
+// item_add:function(frm, cdt, cdn){
+//   calculate_linetotal(frm, cdt,cdn);
+// },
+// item_remove:function(frm, cdt, cdn){
+//   calculate_linetotal(frm, cdt,cdn);
+// },
+//
+//
+//  //  item_remove:function(frm, cdt, cdn){
+//  //    calculate_totvalue(frm, cdt,cdn);
+//  //
+//  // },
+// 	// co4unitpr: function(frm, cdt, cdn){
+// 	// 	calculate_linetotal(frm, cdt, cdn);
+//  //      calculate_totvalue(frm, cdt,cdn);
+// 	// },
+//  //
+//  //  co3totval: function(frm,cdt,cdn){
+//  //    calculate_totvalue(frm, cdt,cdn);
+//  //  },
+// });
--- syma/inventory_management_system/doctype/in8000/in8000.py
+++ syma/inventory_management_system/doctype/in8000/in8000.py
@@ -1,8 +1,52 @@
 # Copyright (c) 2022, Syed Malik and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
+# from console import console
 
 class IN8000(Document):
-	pass
+	def validate(self):
+		if self.in8destwh == '':
+			frappe.throw("Please Enter Destination Warehouse")
+		if self.in8supcd == '':
+			frappe.throw("Please Choose Supplier")
+		if self.in8trnsdt == None:
+			frappe.throw("Please Choose date")
+		if self.convvalue <= 0:
+			frappe.throw("Check Conversion Rate")
+	def before_save(self):
+		total_qty = sum(flt(d.in9qty) for d in self.get("item"))
+		for d in self.get("item"):
+			# if d.in9destwh == None:
+			# 	d.in9destwh = self.in8destwh
+			doc = frappe.get_doc('Material', d.in9itmcd)
+			d.in9avcbef = doc.in3curavgc
+			# frappe.msgprint(doc."Material Warehouse".in6curqty)
+			try:
+				child_item = frappe.get_doc("Material Warehouse", {"warehouse_code": d.in9destwh , "parent": d.in9itmcd})
+				child_item.in6curqty = child_item.in6curqty + d.in9qty
+				child_item.save()
+				# self.in8docref=child_item.in6curqty
+			except frappe.DoesNotExistError:
+				frappe.clear_messages()
+				row = doc.append("qty_in_warehouse", {})
+				row.warehouse_code= d.in9destwh
+				row.in6curqty = d.in9qty
+				doc.save()
+				# frappe.msgprint("Added New Row")
+				# docnew = frappe.get_doc({'doctype': 'Material Warehouse','warehouse_code': self.in8destwh})
+				 # , 'in6curqty': d.in9qty})
+				# docnew.insert()
+				# self.fail(f"Template of {d.in9itmcd}, {d.in9itmname} not published")
+			# frappe.msgprint(doc.in3curavgc)
+		frappe.reload_doctype("in9000")
+
+		# total_qty = self.item(in9itmcd)
+		# total_qty = sum(flt(d.in9qty) for d in self.item)
+		# frappe.msgprint("total_qty")
+
+# def total_Qty(self):
+# 	total_qty=0
+# 	total_qty = sum(flt(d.in9qty) for d in self.get("item"))
